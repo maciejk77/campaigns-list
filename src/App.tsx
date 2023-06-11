7077f5eea96d429020f4e65bb3acf84fc8dd8921
Maciej Kurek <maciejk77@gmail.com>
2023-06-11 15:57:01 +0100

    Inital, clean up, basic table

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import campaigns from './data.json';
+
+type TCampaign = {
+  id: number;
+  name: string;
+  startDate: string;
+  endDate: string;
+  Budget: number;
+};
+
+function createData(
+  id: number,
+  name: string,
+  startDate: string,
+  endDate: string,
+  Budget: number
+) {
+  return { id, name, startDate, endDate, Budget };
 }
 
-export default App;
+const rows = campaigns.reduce(
+  (acc: TCampaign[], { id, name, startDate, endDate, Budget }: TCampaign) => {
+    return [...acc, createData(id, name, startDate, endDate, Budget)];
+  },
+  []
+);
+
+export default function BasicTable() {
+  return (
+    <TableContainer component={Paper} sx={{ margin: 5, maxWidth: '90%' }}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>ID</TableCell>
+            <TableCell align="left">Name</TableCell>
+            <TableCell align="left">Start Date</TableCell>
+            <TableCell align="left">End Date</TableCell>
+            <TableCell align="left">Budget</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row: TCampaign) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.id}
+              </TableCell>
+              <TableCell align="left">{row.name}</TableCell>
+              <TableCell align="left">{row.startDate}</TableCell>
+              <TableCell align="left">{row.endDate}</TableCell>
+              <TableCell align="left">{row.Budget}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
